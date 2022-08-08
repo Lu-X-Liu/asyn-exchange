0d1acb21045ae3885ce41d8680e03c0637215fe7
Lu-X-Liu <91901553+Lu-X-Liu@users.noreply.github.com>
2022-08-08 18:10:42 -0500

    setups

--- index.js
+++
@@ -1,4 +0,0 @@
-
-// 1st function - get ExchangeRate
-// 2nd function - getCountries
-// 3rd function - converCurrency
---
+++ js/index.js
@@ -0,0 +1,5 @@
+//import CURRENCY_API_KEY from './currency_apikey.js';
+import 'dotenv/config' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+import express from 'express'
+
+console.log(process.env);
